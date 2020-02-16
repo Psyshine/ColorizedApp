6d8a3112185d8f42f8fc987849b79d0ed8bbdc0d
Andry Prokopenko <isp.fline@gmail.com>
2020-02-16 15:31:06 +0200

    UI Done

--- ColorizedApp/ViewControllers/ColorSetupViewController.swift
+++ ColorizedApp/ViewControllers/ColorSetupViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CollorSetupViewController.swift
 //  ColorizedApp
 //
 //  Created by Andry Pro on 16.02.2020.
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ColorSetupViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
+    
+    
 
-
+   
 }
-
---
+++ ColorizedApp/ViewControllers/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ColorizedApp
+//
+//  Created by Andry Pro on 16.02.2020.
+//  Copyright © 2020 Andry Pro. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func unwindToColorSetup(_ unwindSegue: UIStoryboardSegue) {
+       
+    }
+}
+
